434b84829878a9398d64a7d17a4f14d438541d99
Scrunshes <scrunshes@gmail.com>
2014-10-20 12:04:35 +0000

    Policy

--- config/policies.js
+++ config/policies.js
@@ -19,33 +19,26 @@
 
 module.exports.policies = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions (`true` allows public     *
-  * access)                                                                  *
-  *                                                                          *
-  ***************************************************************************/
+  '*': false, //by default accept no request
+  /**
+  * User
+  *
+  *
+  */
+  UserController : {
+    'handleLogin'   :   true,
+    'handleLogout'  :   ['userAuth']
+  },
+  /**
+  * Contact CRUD
+  *
+  *
+  */
+  ContactController : {
+    'find'    : ['userAuth'], 
+    'create'  : ['userAuth'],
+    'update'  : ['userAuth'],
+    'destroy' : ['userAuth']
+  }
 
-  // '*': true,
-
-  /***************************************************************************
-  *                                                                          *
-  * Here's an example of mapping some policies to run before a controller    *
-  * and its actions                                                          *
-  *                                                                          *
-  ***************************************************************************/
-	// RabbitController: {
-
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		// '*': false,
-
-		// For the action `nurture`, apply the 'isRabbitMother' policy
-		// (this overrides `false` above)
-		// nurture	: 'isRabbitMother',
-
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		// feed : ['isNiceToAnimals', 'hasRabbitFood']
-	// }
 };
