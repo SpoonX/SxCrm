2c6b61a3d669dcf03644aca46ca6f23db4b093cc
Scrunshes <scrunshes@gmail.com>
2014-10-20 09:15:56 +0000

    Set up Contact Controller

---
+++ api/controllers/ContactController.js
@@ -0,0 +1,11 @@
+/**
+ * ContactController
+ *
+ * @description :: Server-side logic for managing Contacts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
