2f2425dac6230998ea25f5a417cb601ad01bfd6d
Scrunshes <scrunshes@gmail.com>
2014-10-20 10:33:50 +0000

    User Auth policy for contact access

--- api/policies/userAuth.js
+++ api/policies/userAuth.js
@@ -3,7 +3,6 @@
  *
  * @module      :: Policy
  * @description :: Simple policy to allow any authenticated user
- *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`
  * @docs        :: http://sailsjs.org/#!documentation/policies
  *
  */
@@ -11,7 +10,7 @@ module.exports = function(req, res, next) {
 
   // User is allowed, proceed to the next policy, 
   // or if this is the last policy, the controller
-  if (req.session.authenticated) {
+  if (req.session.user) {
     return next();
   }
 
