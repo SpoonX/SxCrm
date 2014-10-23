227beec8b388879bad4ccb85093ec5378f4af030
Scrunshes <scrunshes@gmail.com>
2014-10-23 14:57:42 +0000

    Add addresses model

---
+++ api/models/Address.js
@@ -0,0 +1,27 @@
+/**
+* Address.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    country : {
+      type : "string"
+    },
+    state : {
+      type : "string"
+    },
+    city : {
+      type : "string"
+    },
+    street : {
+      type : "string"
+    },
+    streetNumber : {
+      type : "number"
+    }
+  }
+};
