dbca52284f519b1d3242b4ca1379e8a7bee5e153
Scrunshes <scrunshes@gmail.com>
2014-10-24 12:46:44 +0000

    Add branch & company controllers

---
+++ api/controllers/BranchController.js
@@ -0,0 +1,10 @@
+/**
+ * BranchController
+ *
+ * @description :: Server-side logic for managing Branches
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
